aeb018f59c14348a1d0dcf018784daa77606b546
Abijesh Greg <>
2021-04-08 20:42:33 +0000

    Added fonts

---
+++ main.py
@@ -0,0 +1,12 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+
+img = Image.open("birthday_template.jpg")
+draw = ImageDraw.Draw(img)
+
+# font = ImageFont.truetype(<font-file>, <font-size>)
+font = ImageFont.truetype("montserrat.ttf", 16)
+# draw.text((x, y),"Sample Text",(r,g,b))
+draw.text((0, 0),"Sample Text",(255,255,255),font=font)
+img.save('sample-out.jpg')
