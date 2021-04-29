2e7808a9f70e9303245286dc2056b746d9e9fc44
Abijesh Greg <>
2021-04-29 16:42:45 +0000

    added name text

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ img = Image.open("birthday_template.jpg")
 draw = ImageDraw.Draw(img)
 
 # font = ImageFont.truetype(<font-file>, <font-size>)
-font = ImageFont.truetype("montserrat.ttf", 16)
+font = ImageFont.truetype("Fonts/Montserrat-Black.ttf", 65)
 # draw.text((x, y),"Sample Text",(r,g,b))
-draw.text((0, 0),"Sample Text",(255,255,255),font=font)
+draw.text((45,520),"Sample guy",(255,255,255),font=font)
+
+# font = ImageFont.truetype(<font-file>, <font-size>)
+font = ImageFont.truetype("Fonts/Montserrat-Black.ttf", 16)
+# draw.text((x, y),"Sample Text",(r,g,b))
+draw.text((10, 0),"Feb 31",(255,255,255),font=font)
+
 img.save('sample-out.jpg')
